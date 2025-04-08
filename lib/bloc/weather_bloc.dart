1d361a0c360bf07fb30b984242c4cb1d329052cb
Ved Patel <ved09003@gmail.com>
2025-04-08 13:05:36 +0530

    Implemented City Search in BLoC

---
+++ lib/bloc/weather_bloc.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather_app/data/repository/weather_repository.dart';
+import 'package:flutter_weather_app/models/weather_model.dart';
+
+part 'weather_event.dart';
+part 'weather_state.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+
+  final WeatherRepository weatherRepository;
+  WeatherBloc(this.weatherRepository) : super(WeatherInitial()) {
+    on<WeatherFetchedEvent>(_onWeatherFetchedEvent);
+  }
+
+  // event handler
+  void _onWeatherFetchedEvent(
+    WeatherFetchedEvent event,
+    Emitter<WeatherState> emit,
+  ) async {
+    emit(WeatherLoading());
+      try {
+        final weather = await weatherRepository.getCurrentWeatherData();
+        emit(WeatherSuccess(weatherModel: weather));
+      } catch (e) {
+        emit(WeatherFailure(e.toString()));
+      }
+
+  }
+}
